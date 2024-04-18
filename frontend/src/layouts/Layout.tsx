46b5deaa647f4da247e0403aca36bd4c9b6d30c8
Cleve-codes <clevemomanyi@gmail.com>
2024-04-18 08:49:17 +0300

    header

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,15 +1,19 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import Header from "@/components/Header";
 
 type Props = {
-
+  children: React.ReactNode
 }
 
-function Layout({}: Props) {
-  return (
-    <Button>Layout</Button>
-  )
+const Layout = ({children}: Props) => {
+    return (
+      <div className="flex flex-col min-h-screen">
+        <Header/>
+
+        <div className="container mx-auto flex-1 py-10" >
+          {children}
+        </div>
+      </div>
+    )
 }
 
-
-export default Layout
+export default Layout;
