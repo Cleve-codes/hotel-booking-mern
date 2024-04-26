d1b19a7dfac5599fdeea0aaa2ee7c8d14d2f9420
Cleve-codes <clevemomanyi@gmail.com>
2024-04-26 07:07:36 +0300

    nav

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -2,27 +2,19 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 
 function MainNav() {
-
-  const {loginWithRedirect} = useAuth0()
+  const { loginWithRedirect } = useAuth0();
 
   return (
     <>
-    <Button
-      variant="ghost"
-      className="font-bold hover:text-orange-500"
-      onClick={async () => await loginWithRedirect()}
+      <Button
+        variant="ghost"
+        className="font-bold hover:text-orange-500"
+        onClick={async () => await loginWithRedirect()}
       >
-      Login
-    </Button>
-    <Button
-    variant="ghost"
-    onClick={async () => await loginWithRedirect()}
-    className="font-bold hover:text-orange-500" >
-
-      Logout
-    </Button>
-      </>
-  )
+        Login
+      </Button>
+    </>
+  );
 }
 
 export default MainNav;
