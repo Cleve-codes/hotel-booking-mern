6ce7573e50766e96cf4721190ddd443037a1a54c
Cleve-codes <clevemomanyi@gmail.com>
2024-04-30 13:30:16 +0300

    changes

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,16 @@
+const Login = () => {
+  return (
+    <form>
+      <div>
+        <label>Email</label>
+        <input type="email" placeholder="Email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password" placeholder="Input password" />
+      </div>
+    </form>
+  )
+}
+
+export default Login;
