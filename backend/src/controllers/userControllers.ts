474c003a1d51405cd47cd5a346e053835dc57882
Cleve-codes <clevemomanyi@gmail.com>
2024-05-11 22:27:23 +0300

    jwt

--- backend/src/controllers/userControllers.ts
+++ backend/src/controllers/userControllers.ts
@@ -27,13 +27,21 @@ const createCurrentUser = async (req: Request, res: Response, next: NextFunction
 const updateCurrentUser = async(req: Request, res: Response, next: NextFunction) =>  {
   try {
 
-    const { addressLine1, name, city, country } = req.body;
+    const { name, addressLine1, city, country } = req.body;
     const user = await User.findById({req.userId})
 
     if(!user) {
-      return res.status(404).json("User not found")
+      return res.status(404).json({ message: "User not found" });
     }
 
+    user.name = name;
+    user.addressLine1 = addressLine1;
+    user.city = city;
+    user.country = country;
+
+    await user.save()
+
+    res.send(user)
   } catch (error) {
     console.log(error)
     res.status(500).json({message : "Error updating user"})
--- backend/src/middleware/Auth.ts
+++ backend/src/middleware/Auth.ts
@@ -1,7 +1,23 @@
+import { NextFunction } from "express";
 import { auth } from "express-oauth2-jwt-bearer";
+import { Request, Response, NextFunction } from "express"
 
 export const jwtCheck = auth({
   audience: process.env.AUTH0_AUDIENCE,
   issuerBaseURL: process.env.AUTH0_BASE_ISSUER_URL,
   tokenSigningAlg: 'RS256'
 });
+
+export const jwtParse = async (req: Request, res: Response, next: NextFunction) => {
+
+    const { authorization } = req.headers;
+
+    if(!authorization || !authorization.startsWith("Bearer ")) {
+      return res.sendStatus(401);
+    }
+
+
+    const token = authorization.split(" ")[1];
+
+
+}
