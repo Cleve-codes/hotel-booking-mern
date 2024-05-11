fdb64cc79883e4e03bfcded52bcf4e73ab421edc
Cleve-codes <clevemomanyi@gmail.com>
2024-05-11 22:44:23 +0300

    put

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,10 +1,10 @@
 import express from "express"
 import userControllers from "../controllers/userControllers";
-import { jwtCheck } from "../middleware/Auth";
+import { jwtCheck, jwtParse } from "../middleware/Auth";
 
 const router = express.Router();
 
 router.post("/", jwtCheck, userControllers.createCurrentUser)
-router.put("/", userControllers.updateCurrentUser)
+router.put("/", jwtParse ,userControllers.updateCurrentUser)
 
 export default router;
